c8446f881f5b04d09a451d13e3f83d8782aef6a0
soumyat <soumyat@users.noreply.github.com>
2015-04-25 22:55:18 +0530

    Update CacheMatrix.R

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -1,10 +1,13 @@
+# Function to create special matrix object to cache its inverse #
 makeCacheMatrix <- function(x = matrix()) {
+  #Set matrix inverse to NULL
   minv <- NULL
   set <- function(y) {
     x <<- y
     minv <<- NULL
   }
   get <- function() x
+  #Inverse of matrix is calculated using solve function
   setinverse<-function(solve) minv<<-solve
   getinverse<-function()minv
   list(set=set,get=get, setinverse=setinverse,
